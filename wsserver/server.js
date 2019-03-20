83a5e45f470b4c2a6239ffed8eb0e707fc2322f1
dmitriy.dev <info@devstudio.kz>
2019-03-20 20:25:21 +0600

    fix

--- wsserver/server.js
+++
@@ -1,9 +0,0 @@
-var io = require('socket.io')(6001);
-io.on('connection', function (socket) {
-    console.log(socket.id);
-
-    socket.send('Message from server');
-    socket.emit('message', 'Version');
-
-    socket.broadcast.send('New user');
-});
