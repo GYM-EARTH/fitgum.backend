22d9d4545338623635f443ecbb4dd4ad426e1540
dmitriy.dev <info@devstudio.kz>
2019-04-21 15:28:48 +0600

    Created Programs

--- database/migrations/2019_04_20_103745_create_program_day_schedules_table.php
+++ database/migrations/2019_04_20_103745_create_program_day_schedules_table.php
@@ -16,7 +16,7 @@ class CreateProgramDayScheduleSTable extends Migration
         Schema::create('program_day_schedules', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $table->text('data')->default('{}');
+            $table->text('data')->nullable();
             $table->timestamps();
         });
     }
